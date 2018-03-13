bed37bc0a4148388720c9ea17122b8e3a0a2f396
Luiz Felipe <felipe.silva337@yahoo.com>
2018-03-13 17:28:21 -0300

    Create sort.h

---
+++ insertion-sort/inc/sort.h
@@ -0,0 +1,16 @@
+/********************
+ * Developed by Luiz Felipe
+ * GitHub: https://github.com/Silva97
+ *
+ * Distributed under the MIT License.
+ ********************/
+
+#ifndef _SORT_H
+#define _SORT_H
+
+void   intarray_sort(int *arr, int size);
+void   strarray_sort(char **arr, int size);
+char** strarray_alloc(int w, int h);
+void   strarray_set(char **arr, int length, ...);
+
+#endif
